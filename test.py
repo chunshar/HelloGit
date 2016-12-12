be21d5350045d8c4d213f87034aee3abc301cfe0
chunshar <chunsha@me.com>
2016-12-12 12:50:44 +0000

    n=256->512

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 def f(x, y):
     return (1 - x/2 + x**5 + y**3) * np.exp(-x**2 -y**2)
 
-n = 256
+n = 512
 x = np.linspace(-3, 3, n)
 y = np.linspace(-3, 3, n)
 X, Y = np.meshgrid(x, y)
