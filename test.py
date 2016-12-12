b057bd5def04b99e646441f0d9f7d339174f89c5
chunshar <chunsha@me.com>
2016-12-12 20:46:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,13 +9,13 @@ matplotlib.
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x,y):
-    return (1 - x / 2 + x**5 + y**3) * np.exp(-x**2 -y**2)
+def f(x, y):
+    return (1 - x/2 + x**5 + y**3) * np.exp(-x**2 -y**2)
 
 n = 256
 x = np.linspace(-3, 3, n)
 y = np.linspace(-3, 3, n)
-X,Y = np.meshgrid(x, y)
+X, Y = np.meshgrid(x, y)
 
 plt.axes([0.025, 0.025, 0.95, 0.95])
 
@@ -26,4 +26,3 @@ plt.clabel(C, inline=1, fontsize=10)
 plt.xticks(())
 plt.yticks(())
 plt.show()
-
